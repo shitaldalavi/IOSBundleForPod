3cb6b1e83f77d9e699429769f8030514dd1b70d3
shital.dalavi <shital.dalavi@nciportal.com>
2021-08-06 16:34:39 +0530

    Changes

---
+++ IOSBundleForPod/IOSBundleForPod 2021-08-06 16-33-15/IOSBundleForPod 06-08-21, 4.33 PM.xcarchive/Products/Library/Frameworks/IOSBundleForPod.framework/Headers/IOSBundleForPod.h
@@ -0,0 +1,18 @@
+//
+//  IOSBundleForPod.h
+//  IOSBundleForPod
+//
+//  Created by admin on 06/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOSBundleForPod.
+FOUNDATION_EXPORT double IOSBundleForPodVersionNumber;
+
+//! Project version string for IOSBundleForPod.
+FOUNDATION_EXPORT const unsigned char IOSBundleForPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOSBundleForPod/PublicHeader.h>
+
+
---
+++ IOSBundleForPod/LoginCLass.swift
@@ -0,0 +1,23 @@
+//
+//  LoginCLass.swift
+//  IOSBundleForPod
+//
+//  Created by admin on 06/08/21.
+//
+
+import Foundation
+
+public class LoginClass{
+    private init()
+    {
+        print("This is what actually wanteddddd.......")
+    }
+    public static func PrintData()->String
+    {
+      
+        let data = "Yesss worked...."
+       return data
+    }
+}
+
+
